698eabdfcd58b21bcd2a9185a8d653f5060c41e6
aruslurya <100155362+aruslurya@users.noreply.github.com>
2022-08-10 21:10:33 -0400

    Code Changes

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using MagicVilla_VillaAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : Controller
+    {
+        [HttpGet]
+       public IEnumerable<Villa> GetVillas()
+       {
+            return new List<Villa>
+            {
+                new Villa{Id=1,Name = "Pool View"},
+                new Villa{Id = 2, Name = "Beach View"}
+            };
+       }
+    }
+}
